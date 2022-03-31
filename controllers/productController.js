b6fef7cb99a8bcc184d73c26c680aa51e3f48d88
Mauricio Lopes <mlopeesz@gmail.com>
2022-03-31 14:04:48 -0300

    feat: implementa função getAll em productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,12 @@
-// Renomeie esse arquivo
+const productService = require('../services/productService');
+
+const getAll = async (_req, res) => {
+  try {
+    const products = await productService.getAll();
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = getAll;
