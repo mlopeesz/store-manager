2a88080849d1a4117cbf5f6cb176cf5419829930
Mauricio Lopes <mlopeesz@gmail.com>
2022-03-31 13:56:28 -0300

    feat: adiciona função getAll em productService

--- services/productService.js
+++ services/productService.js
@@ -1,1 +1,15 @@
-// Renomeie esse arquivo
+const productModel = require('../models/productModel');
+
+const getAll = async () => {
+  try {
+      const products = await productModel.getAll();
+      return products;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Database problems');
+    }
+};
+
+module.exports = {
+  getAll,
+};
