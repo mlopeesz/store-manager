1ade9030459918878885d3cc12bf0d24af2dc5ec
Mauricio Lopes <mlopeesz@gmail.com>
2022-03-31 12:06:07 -0300

    feat: função para selecionar todos os produtos

---
+++ models/productModel.js
@@ -0,0 +1,8 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const [products] = await connection.execute('SELECT * FROM StoreManager.products');
+    return products;
+};
+
+module.exports = getAll;
