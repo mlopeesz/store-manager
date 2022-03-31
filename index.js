402ad72b2d559dd0e2396a1e127d958a72a928b2
Mauricio Lopes <mlopeesz@gmail.com>
2022-03-31 14:20:20 -0300

    feat: importa rotas em index

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const express = require('express');
+const productRoutes = require('./routes/productRotes');
+
+const app = express();
 require('dotenv').config();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.use('/products', productRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
